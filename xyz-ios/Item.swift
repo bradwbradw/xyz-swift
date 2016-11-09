32430589c96a8f78d00eff34a1d2aee4a11edb71
brad winter <braddjwinter@gmail.com>
2016-11-08 19:30:41 -0800

    wip

---
+++ xyz-ios/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  xyz-ios
+//
+//  Created by Bradley Winter on 11/8/16.
+//  Copyright © 2016 Bradley Winter. All rights reserved.
+//
+
+
+class Item {
+    //    var id: String
+    var title: String
+    //    var artist: String
+    //    var provider: String
+    //    var provider_id: String
+    
+    
+    init(title: String){
+        self.title = title
+    }
+    
+}
+
